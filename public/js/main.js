8af24718a097c5d126f8f8efadc08c16cdf75e01
Adnan Sadiq <sadiqadnan109@gmail.com>
2022-06-07 15:35:49 +0530

    second commit

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,14 @@ const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
 // Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
+const {username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
 const socket = io();
 
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit('joinRoom', {username, room });
 
 // Get room and users
 socket.on('roomUsers', ({ room, users }) => {
@@ -62,6 +62,5 @@ function outputRoomName(room) {
 // Add users to DOM
 function outputUsers(users) {
   userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join('')}
-  `;
+    ${users.map(user => `<li>${user.username}</li>`).join('')}`;
 }
